8881db66dc5964b0d8442c02c99a297b20df5d01
huangbaoming <huangbm@dadaodata.com>
2019-04-20 10:37:09 +0800

    基础页面结构

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'PagesController@root')->name('root');
 
-Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
+//Route::get('/', 'StaticPagesController@home')->name('home');
+//Route::get('/help', 'StaticPagesController@help')->name('help');
+//Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('signup', 'UsersController@create')->name('signup');
+//Route::get('signup', 'UsersController@create')->name('signup');
 
 
 Route::resource('users', 'UsersController');
