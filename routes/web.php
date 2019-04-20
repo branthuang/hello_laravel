d70e7b49a1e5b6c4f2fa205f48ccc5201a591857
huangbaoming <huangbm@dadaodata.com>
2019-04-20 14:34:38 +0800

    登录注册

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+r<?php
 
 /*
 |--------------------------------------------------------------------------
@@ -12,18 +12,24 @@
 */
 Route::get('/', 'PagesController@root')->name('root');
 
-//Route::get('/', 'StaticPagesController@home')->name('home');
-//Route::get('/help', 'StaticPagesController@help')->name('help');
-//Route::get('/about', 'StaticPagesController@about')->name('about');
+//Auth::routes();
 
-//Route::get('signup', 'UsersController@create')->name('signup');
+// 用户身份验证相关的路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
+// 用户注册相关路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
 
-Route::resource('users', 'UsersController');
-//Route::get('/users', 'UsersController@index')->name('users.index');
-//Route::get('/users/create', 'UsersController@create')->name('users.create');
-//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
-//Route::post('/users', 'UsersController@store')->name('users.store');
-//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
-//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
-//Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+// 密码重置相关路由
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// Email 认证相关路由
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
